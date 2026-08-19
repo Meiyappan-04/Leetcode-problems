2778615f1aa4179ace87e2e34669c06259809fa1
Meiyappan Chidambaram <meiyappan04032007@gmail.com>
2026-08-19 12:05:17 +0530

    Time: 0 ms (100%), Space: 43.7 MB (63.77%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -7,18 +7,16 @@ class Solution {
             if(i<0){
                 nums1[k]=nums2[j];
                 j--;
-                k--;
             }
             else if(nums1[i]<=nums2[j]){
                 nums1[k]=nums2[j];
                 j--;
-                k--;
             }
             else{
                 nums1[k]=nums1[i];
                 i--;
-                k--;
             }
+            k--;
         }
     }
 }
